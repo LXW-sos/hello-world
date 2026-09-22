66da1ab15dee362f1408794081681a2c7b4443ea
LXW-sos <2106727737qq.com>
2026-09-22 12:55:47 +0800

    fix swap:move exit condition before swap, test passed

--- c_practice/swap.c
+++ c_practice/swap.c
@@ -10,10 +10,19 @@ void swap(int *a, int *b)
 int main()
 {
     int x, y;
-    printf("请输入两个整数：");
-    scanf("%d %d", &x, &y);
-    printf("交换前：x=%d, y=%d\n", x, y);
-    swap(&x, &y);
-    printf("交换后：x=%d, y=%d\n", x, y);
+
+    while(1)
+    {
+        printf("Enter two numbers(0 0 to exit): ");
+        scanf("%d %d", &x, &y);
+
+        if(x == 0 && y == 0)
+        {
+            break;
+        }
+        swap(&x, &y);
+        printf("After swap : x = %d, y = %d\n\n", x, y);
+    }
     return 0;
 }
+   
